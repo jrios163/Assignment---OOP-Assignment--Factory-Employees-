aff5a679d6080b79318564c3aa62910fc9daa03c
jose <riosjose602@gmail.com>
2021-08-09 21:16:58 -0500

    First Assignmet

---
+++ Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_13
+{
+    class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        private decimal _salary;
+
+        //con
+        public Employee (string firstName, string lastName, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Salary = salary;
+
+        }
+
+        public decimal Salary
+        {
+            get { return _salary; }
+            set
+            {
+                if (value >= 0)
+                    _salary = value;
+            }
+        }
+    }
+}
